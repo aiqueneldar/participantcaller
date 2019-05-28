81709acad8a36d9612bb05086e29d96c59b87d53
Aiquen <aiqueneldar@gmail.com>
2019-05-28 23:41:29 +0200

    Revamped most of the code and structure to have one Lambda function that will deal with all the HTTP Verbs when it comes to events.
    Signed-off-by: Aiquen <aiqueneldar@gmail.com>

---
+++ api_events.py
@@ -0,0 +1,115 @@
+import pymysql
+import json
+import os
+import sys
+import logging
+import boto3
+import base64
+from botocore.exceptions import ClientError
+from pymysql.err import *
+
+def get_secret(env):
+
+    secret_name = str(env) + "/partcaller/database"
+    region_name = "eu-west-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    # In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+    # See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    # We rethrow the exception by default.
+
+    try:
+        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'DecryptionFailureException':
+            # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InternalServiceErrorException':
+            # An error occurred on the server side.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidParameterException':
+            # You provided an invalid value for a parameter.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'InvalidRequestException':
+            # You provided a parameter value that is not valid for the current state of the resource.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+        elif e.response['Error']['Code'] == 'ResourceNotFoundException':
+            # We can't find the resource that you asked for.
+            # Deal with the exception here, and/or rethrow at your discretion.
+            raise e
+    else:
+        # Decrypts secret using the associated KMS CMK.
+        # Depending on whether the secret is a string or binary, one of these fields will be populated.
+        if 'SecretString' in get_secret_value_response:
+            return get_secret_value_response['SecretString']
+        else:
+            return base64.b64decode(get_secret_value_response['SecretBinary'])
+
+env = os.getenv("env", "dev")
+json_secret = get_secret(env)
+config = json.loads(json_secret)
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn = pymysql.connect(host=config['host'], user=config['user'], passwd=['password'], db="PartcallerDB",
+                           connect_timeout=5)
+except:
+    logger.error("ERROR: Couldn't connect to database!")
+    sys.exit(1)
+
+logger.info("SUCCESS: Connection established to database")
+
+def list_events(x):
+    cursor = None
+    try:
+        cursor = conn.cursor()
+    except (OperationalError, InternalError) as err:
+        logger.error("Couldn't get database connection while listing events")
+        raise err
+
+    rows = []
+    try:
+        cursor.execute("SELECT * FROM Events")
+        rows = cursor.fetchall()
+    except (OperationalError) as err:
+        logger.error("Couldn't list all events. Problem with DB")
+        raise err
+
+    return rows
+
+def handler(event, context):
+    '''Provide an event that contains the following keys:
+
+          - operation: one of the operations in the operations dict below
+          - payload: a parameter to pass to the operation being performed
+        '''
+    logger.debug("Received event: " + json.dumps(event, indent=2))
+
+    operation = ""
+    try:
+        operation = event['httpMethod']
+    except KeyError as err:
+        logger.error("No operation submitted")
+        raise err
+
+    operations = {
+        "GET": list_events,
+    }
+    payload = event.get('body', {})
+    payload = json.loads(payload)
+    if operation in operations:
+        return operations[operation](payload)
+    else:
+        raise ValueError('Unrecognized operation "{}"'.format(operation))
